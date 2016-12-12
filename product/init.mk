1d1394ae8c63237012054bb65fb958b29c7b2933
rohan <purohit.rohan@gmail.com>
2016-12-12 10:03:23 +0530

    rootdir: rework

--- product/init.mk
+++ product/init.mk
@@ -4,3 +4,8 @@ PRODUCT_PACKAGES += \
     init.qcom.fm.sh \
     init.qcom.sh \
     init.target.rc
+
+# Temp, until we find a better way
+PRODUCT_COPY_FILES += \
+   device/xiaomi/ferrari/rootdir/etc/ueventd.qcom.rc:root/ueventd.qcom.rc \
+   device/xiaomi/ferrari/rootdir/etc/init.qcom.usb.rc:root/init.qcom.usb.rc
